af123d7d0fac283ee9d85df6743cc3dfc5bcb2b9
Kanan <kananpretty@gmail.com>
2026-01-14 23:06:53 +0400

    updated some settings

--- TaskTrackerServer/app.js
+++ TaskTrackerServer/app.js
@@ -1,17 +1,39 @@
 import express from "express";
 import taskRoutes from "./routes/tasks.route.js";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
 /* middleware */
+app.use(helmet());
 app.use(express.json()); // Parses JSON in request body
 app.use(express.urlencoded({ extended: true })); // Optional: parses form data
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 /* routes */
 app.use("/api/tasks", taskRoutes);
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 export default app;
--- TaskTrackerServer/config/db.js
+++ TaskTrackerServer/config/db.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
-
+const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://kanan:kanansharmehta90@clustertasktracker.gukzqpb.mongodb.net/?appName=ClusterTaskTracker"
-    );
+    const conn = await mongoose.connect(MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
